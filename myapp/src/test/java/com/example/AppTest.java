e20170dbbe030b474895b5d66286f4f26448d43f
Omar <omaralkadri4@gmail.com>
2025-06-04 19:58:54 +0200

    modified pom.xml and maven.yml

--- myapp/src/test/java/com/example/AppTest.java
+++ myapp/src/test/java/com/example/AppTest.java
@@ -1,20 +1,12 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+public class AppTest {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void testAppWorks() {
+        assertEquals(2, 1 + 1);
     }
 }
