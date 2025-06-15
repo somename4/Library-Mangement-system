fa87bd8fe6675efecbf1fac006fd3c14cf7f59ff
omar <omaralkadri4@gmail.com>
2025-06-15 14:11:12 +0200

    tested the printer class in the main

--- myapp/src/main/java/app/LibraryApp.java
+++ myapp/src/main/java/app/LibraryApp.java
@@ -1,8 +1,15 @@
 package app;
 
+import services.CustomerInfoPrintert;
+import services.CustomerInitiator;
+
 public class LibraryApp {
     
     public static void main(String[] args) {
+        CustomerInitiator ci = new CustomerInitiator();
+        CustomerInfoPrintert cp = new CustomerInfoPrintert();
+        cp.printMultipleCustomer(CustomerInitiator.CUSTOMERS);        
+
 /*         CustomerInitiator ci = new CustomerInitiator();
         BookIntiator bi = new BookIntiator();
         BookCopyInitiator bci = new BookCopyInitiator();
